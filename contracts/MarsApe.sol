6fb5d70e7a5eb2a53d09fe8e3ba2bfd5ad9a5999
DuongNguyen2 <duong.nguyen2@sotatek.com>
2021-12-06 14:43:03 +0700

    ...

--- contracts/MarsApe.sol
+++ contracts/MarsApe.sol
@@ -5,7 +5,6 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./ApeAccountant.sol";
-import "hardhat/console.sol";
 
 contract MarsApe is ERC20, ERC20Burnable, Ownable, ApeAccountant {
 
